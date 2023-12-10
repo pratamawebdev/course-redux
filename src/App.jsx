dfcae49f9454263ec69b17ee4556bf7e6e38eac4
willy <willypratama231@gmail.com>
2023-12-10 17:59:54 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import ProductList from "./features/productlist/Productlist";
+import CartModal from "./features/cart/CartModal";
+
+function App() {
+  const [isOpenModalCart, setIsOpenModalCart] = useState(false);
+
+  const handleOpenModalCart = () => {
+    setIsOpenModalCart(true);
+  };
+
+  const handleHideModalCart = () => {
+    setIsOpenModalCart(false);
+  };
+
+  return (
+    <>
+      {isOpenModalCart ? (
+        <CartModal handleHideModalCart={handleHideModalCart} />
+      ) : null}
+      <Header handleOpenModalCart={handleOpenModalCart} />
+      <main className="max-w-7xl mx-auto px-4">
+        <ProductList />
+      </main>
+    </>
+  );
+}
+
+export default App;
